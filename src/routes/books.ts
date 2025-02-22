5f3f1aea249a008c6c3563e035c81d88861af2e9
Tamires Manolio Vanucci <tamiresvanucci.2@gmail.com>
2025-02-22 16:53:24 -0300

    feat: :sparkles: Criei o controle do updateBook

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -3,11 +3,12 @@ import { expressRouteAdapter } from "../adapters/expressRouteAdapter";
 import { addBookControllerFactory } from "../factories/books/addBookControllerFactory";
 import { deleteBookControllerFactory } from "../factories/books/deleteBookControllerFactory";
 import { listBookControllerFactory } from "../factories/books/listBookControllerFactory";
+import { updateBookControllerFactory } from "../factories/books/updateBookControllerFactory";
 
 export default (router: Router): void => {
     router.get('/books', expressRouteAdapter(listBookControllerFactory()));
     router.post('/books', expressRouteAdapter(addBookControllerFactory()));
     router.get('/books/:id');
-    router.put('/books/:id');
+    router.put('/books/:id', expressRouteAdapter(updateBookControllerFactory()));
     router.delete('/books/:id', expressRouteAdapter(deleteBookControllerFactory()));
 }
