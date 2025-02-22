faa68c039ef00a27283656f0cddb0df737ba277d
Adriel Renato <adriel.renato26@gmail.com>
2025-02-22 15:15:48 -0300

    feat: :sparkles: foi implementado o list book, porém ainda não implementei o book repositorio

--- src/controllers/books/listBookController.ts
+++ src/controllers/books/listBookController.ts
@@ -1,12 +1,19 @@
 import { Controller } from "../../interfaces/controller";
 import { HttpRequest, HttpResponse } from "../../interfaces/http";
 import { IBookRepository } from "../../repositories/interfaces/interfaceBookRepository";
+import { ok, serverError } from "../../utils/httpResponses/httpResponse";
 
 export class ListBookController implements Controller {
     constructor(private readonly bookRepository: IBookRepository) {}
 
-    handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        throw new Error("Method not implemented.");
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const books = this.bookRepository.list();
+
+            return ok(books);
+        } catch(error: any) {
+            return serverError(error);
+        }
     }
     
 }
