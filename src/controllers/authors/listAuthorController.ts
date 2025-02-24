982bcdaa109a069a0d7da0055c9c197a2c4b9c38
Tamires Manolio Vanucci <tamiresvanucci.2@gmail.com>
2025-02-24 19:03:07 -0300

    feat: :sparkles: Criei o controle do authorBook

--- src/controllers/authors/listAuthorController.ts
+++ src/controllers/authors/listAuthorController.ts
@@ -1,7 +1,18 @@
 import { Controller } from "../../interfaces/controller";
+import { HttpRequest, HttpResponse } from "../../interfaces/http";
 import { IAuthRepository } from "../../repositories/interfaces/interfaceAuthRepository";
+import { ok, serverError } from "../../utils/httpResponses/httpResponse";
 
 export class ListAuthorController implements Controller {
     constructor(private readonly authorRepository: IAuthRepository) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+            try {
+                const authors = await this.authorRepository.list();
     
+                return ok(authors);
+            } catch(error: any) {
+                return serverError(error);
+            }
+        }
 }
--- src/factories/authors/listAuthorControllerFactory.ts
+++ src/factories/authors/listAuthorControllerFactory.ts
@@ -1,9 +1,10 @@
+import { ListAuthorController } from "../../controllers/authors/listAuthorController";
 import { LogErrorControllerDecorator } from "../../decorators/logError";
 import { AuthorRepository } from "../../repositories/authorRepository"
 import LogErrorRepository from "../../repositories/logErrorRepository";
 
 export const listAuthorControllerFactory = () => {
-    const listAuthorController = new listAuthorController(new AuthorRepository());
+    const listAuthorController = new ListAuthorController(new AuthorRepository());
 
     return new LogErrorControllerDecorator(
         listAuthorController,
